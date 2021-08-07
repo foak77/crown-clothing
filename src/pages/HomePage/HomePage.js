a6cedc219afec5919a6e7e0d2ce06dbde037f048
foak77 <emailtofred@gmail.com>
2021-08-07 17:14:02 -0400

    homepage components, styling

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Directory from './../../components/Directory/Directory'
+import "./homepage.scss"
+
+function HomePage() {
+    return (
+        <div className="homepage">
+           <Directory/>
+        </div>
+    )
+}
+
+export default HomePage
